5d69f3ed0d20cd6f78a386feba869cf8340d67c4
Chralu <contact@charly-caulet.net>
2022-08-01 18:41:59 +0200

    Add comments to PokemonPage.

--- lib/pokemon_page/pokemon_page.dart
+++ lib/pokemon_page/pokemon_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:formation_flutter/model/pokemon.dart';
 
+/// Un écran affichant les informations au sujet
+/// d'un [Pokemon]
 class PokemonPage extends StatelessWidget {
   final Pokemon pokemon;
 
@@ -11,6 +13,8 @@ class PokemonPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Pour le moment, on affiche seulement l'identifiant du [Pokemon]
+    // A vous d'ajouter l'affichage des autres informations
     return Center(
       child: Text("${pokemon.id}"),
     );
